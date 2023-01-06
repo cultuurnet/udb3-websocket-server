1f8c27858995c2e1973875dc86f4da16ffc5398e
simon-debruijn <simon.debruijn@hotmail.com>
2023-01-06 16:20:43 +0100

    Remove newline

--- src/getFileDirGlobals.js
+++ src/getFileDirGlobals.js
@@ -3,7 +3,6 @@ import { dirname } from "path";
 
 export const getFileDirGlobals = (meta) => {
   const __filename = fileURLToPath(meta.url);
-
   const __dirname = dirname(__filename);
 
   return { __dirname, __filename };
