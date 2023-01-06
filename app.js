5a40b004a77f22170f739e3001fe2cbeab1a54d6
simon-debruijn <simon.debruijn@hotmail.com>
2023-01-06 15:08:58 +0100

    Fix missing import

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ import { readConfig } from "./readConfig.js";
 import { indexPageHandler } from "./indexPageHandler.js";
 import { initializeSocketIO } from "./io.js";
 
-export const config = await readConfig();
+const config = await readConfig();
 
 const PORT = config.port ?? 3000;
 
-export const httpServer = http.createServer({}, indexPageHandler);
+const httpServer = http.createServer({}, indexPageHandler);
 
-await initializeSocketIO(httpServer);
+await initializeSocketIO(httpServer, config);
 
 httpServer.listen(PORT, () =>
   console.log(`Server listening on port http://localhost:${PORT}`)
--- pathFromRoot.js
+++ pathFromRoot.js
@@ -1,4 +1,5 @@
 import path from "path";
+import { getFileDirGlobals } from "./getFileDirGlobals.js";
 
 export const pathFromRoot = (pathToFile) => {
   const { __dirname } = getFileDirGlobals(import.meta);
